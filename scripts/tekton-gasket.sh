6c1c7d191b4031618bbb63fb70b9c63dd663b5d7
Powell Quiring <powellquiring@gmail.com>
2020-02-12 13:45:17 -0800

    tekton

---
+++ scripts/tekton-gasket.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# The tekton pipeline is calling the same scripts as the non tekton pipeline.
+# This gasket will set up the environment variables that the scripts expect based on the environment
+
